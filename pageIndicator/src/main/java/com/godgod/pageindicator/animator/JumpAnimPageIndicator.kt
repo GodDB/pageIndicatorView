3555df09a0c587cb8e6bd489a0e827b0f1b72d46
GodDB <saqwzx002@naver.com>
2022-03-07 22:15:10 +0900

    점프 애니메이션 스케일 값 조절 0.8 -> 0.6

--- pageIndicator/src/main/java/com/godgod/pageindicator/animator/JumpAnimPageIndicator.kt
+++ pageIndicator/src/main/java/com/godgod/pageindicator/animator/JumpAnimPageIndicator.kt
@@ -17,7 +17,7 @@ internal class JumpAnimPageIndicator : PageIndicatorDrawer() {
 
     private fun drawJumpUpAnim(canvas: Canvas, paint: Paint, itemGap: Int, position: Int, positionOffset: Float, indicator: PageIndicatorView.Indicator) {
         val animValue = positionOffset
-        val scaleAnimValue = 1 - (0.4f *positionOffset)
+        val scaleAnimValue = 1 - (0.8f *positionOffset)
         val distance = indicator.circleRadius*2 + (itemGap / 2)
         val jumpY = -(tan(Math.toRadians(60.0)) * distance).toFloat()
         canvas.drawCircle(indicator.cx + (distance * animValue), indicator.cy + (jumpY * animValue), indicator.circleRadius * scaleAnimValue, paint)
@@ -25,7 +25,7 @@ internal class JumpAnimPageIndicator : PageIndicatorDrawer() {
 
     private fun drawJumpDownAnim(canvas: Canvas, paint: Paint, itemGap: Int, position: Int, positionOffset: Float, indicator: PageIndicatorView.Indicator) {
         val animValue = positionOffset - 0.5f
-        val scaleAnimValue = 0.8f + (0.4f * (positionOffset - 0.5f))
+        val scaleAnimValue = 0.6f + (0.8f * (positionOffset - 0.5f))
         val distance = indicator.circleRadius*2 + (itemGap /2)
         val prevX = distance * 0.5f
         val prevY = -(tan(Math.toRadians(60.0)) * distance).toFloat() * 0.5f
